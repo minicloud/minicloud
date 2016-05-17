cd51cdef112acd34a61d491a5e27b447886c21dc
jim <jim@miniyun.cn>
2016-05-17 12:59:32 +0800

    http->https

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-process.on('uncaughtException', function(err){
-  console.log(`Caught exception: ${err}`)
+var https = require('https')
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+process.on('uncaughtException', function(err) {
+    console.log(`Caught exception: ${err}`)
+    console.log(`Caught exception: ${err.stack}`)
 })
 require('co').wrap(function*() {
     var app = yield require("./lib/loader/app-loader")()
-    app.listen(global.config.port)
+    var options = {
+            key: fs.readFileSync(path.join(__dirname, 'server.key')),
+            cert: fs.readFileSync(path.join(__dirname, 'server.crt'))
+        }
+        // http.createServer(app.callback()).listen(global.config.port)
+    https.createServer(options, app.callback()).listen(global.config.port)
 })()
